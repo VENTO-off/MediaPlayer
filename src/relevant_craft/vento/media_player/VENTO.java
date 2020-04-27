e567096b86923a5d213510d64941c197d33b0a31
VENTO <vento140@mail.ru>
2020-04-27 21:23:37 +0300

    Initial commit
    Signed-off-by: VENTO <vento140@mail.ru>

---
+++ src/relevant_craft/vento/media_player/VENTO.java
@@ -0,0 +1,17 @@
+package relevant_craft.vento.media_player;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class VENTO extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
